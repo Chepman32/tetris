d9dbe0357b990e449f229fc21c870592b2a7d6b2
Chepman32 <antonkerch555@gmail.com>
2021-03-28 15:59:55 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { Video, AVPlaybackStatus } from 'expo-av';
+import Main from './src/main';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class AppContainer extends React.Component {
+  state = {
+    assetsLoaded: false,
+  }
+  render() {
+
+    return (
+      <View style={{flex: 1}}>
+        <Main />
+
+        <Video
+          source={require('./assets/sounds/tetris.mp3')}
+          repeat={true}
+          style={{position: 'absolute', width: 0, height: 0}}
+        />
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default AppContainer
